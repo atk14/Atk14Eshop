56173f1cbd9e7030646b6cd235d2c94ebf6345b4
Jaromir Tomek <jaromir.tomek@gmail.com>
2019-03-05 17:44:12 +0100

    Added warehouses

---
+++ db/migrations/0028_warehouses.sql
@@ -0,0 +1,38 @@
+CREATE SEQUENCE seq_warehouses;
+CREATE TABLE warehouses (
+	id INT PRIMARY KEY DEFAULT NEXTVAL('seq_warehouses'),
+	--
+	code VARCHAR(255),
+	name VARCHAR(255),
+	applies_to_eshop BOOLEAN NOT NULL DEFAULT 't',
+	store_id INT,
+	--
+	rank INT NOT NULL DEFAULT 999,
+	--
+	created_by_user_id INT,
+	updated_by_user_id INT,
+	--
+	created_at TIMESTAMP NOT NULL DEFAULT NOW(),
+	updated_at TIMESTAMP,
+	--
+	CONSTRAINT fk_warehouses_stores FOREIGN KEY (store_id) REFERENCES stores ON DELETE SET NULL,
+	CONSTRAINT fk_warehouses_cr_users FOREIGN KEY (created_by_user_id) REFERENCES users,
+	CONSTRAINT fk_warehouses_upd_users FOREIGN KEY (updated_by_user_id) REFERENCES users
+);
+INSERT INTO warehouses (name,applies_to_eshop) VALUES('Default','t');
+
+CREATE SEQUENCE seq_warehouse_products;
+CREATE TABLE warehouse_products (
+	id INT PRIMARY KEY DEFAULT NEXTVAL('seq_warehouse_products'),
+	--
+	warehouse_id INT NOT NULL,
+	product_id INT NOT NULL,
+	stockcount INT NOT NULL,
+	--
+	created_at TIMESTAMP NOT NULL DEFAULT NOW(),
+	updated_at TIMESTAMP,
+	--
+	CONSTRAINT unq_warehouseproducts UNIQUE (warehouse_id,product_id),
+	CONSTRAINT fk_warehouseproducts_warehouses FOREIGN KEY (warehouse_id) REFERENCES warehouses ON DELETE CASCADE,
+	CONSTRAINT fk_warehouseproducts_products FOREIGN KEY (product_id) REFERENCES products ON DELETE CASCADE
+);
