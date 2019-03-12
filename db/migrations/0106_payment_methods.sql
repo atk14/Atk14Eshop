376201395a7778ee6d4f2fd346d0a5d05d1e173d
Jaromir Tomek <jaromir.tomek@gmail.com>
2019-03-12 14:13:05 +0100

    Fixes in structures for delivery_methods and payment_methods

--- db/migrations/0106_payment_methods.sql
+++ db/migrations/0106_payment_methods.sql
@@ -3,7 +3,7 @@ CREATE TABLE payment_methods (
 	id INT PRIMARY KEY DEFAULT NEXTVAL('seq_payment_methods'),
 	--
 	code VARCHAR(255), -- alternative key
-	region_id INT NOT NULL DEFAULT 1,
+	regions JSON,
 	payment_gateway_id INT, --
 	image_url VARCHAR(255), -- icon
 	price NUMERIC(20,6) NOT NULL DEFAULT 0.0,
@@ -18,7 +18,6 @@ CREATE TABLE payment_methods (
 	updated_at TIMESTAMP,
 	--
 	CONSTRAINT unq_paymentmethods_code UNIQUE (code),
-	CONSTRAINT fk_paymentmethods_regions FOREIGN KEY (region_id) REFERENCES regions,
 	CONSTRAINT fk_paymentmethods_paymentgateways FOREIGN KEY (payment_gateway_id) REFERENCES payment_gateways,
 	CONSTRAINT fk_paymentmethods_cr_users FOREIGN KEY (created_by_user_id) REFERENCES users,
 	CONSTRAINT fk_paymentmethods_upd_users FOREIGN KEY (updated_by_user_id) REFERENCES users
