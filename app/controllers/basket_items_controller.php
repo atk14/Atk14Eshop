4512cdcecb51d077791ed5a738b40f2af7c3126e
Jaromir Tomek <jaromir.tomek@gmail.com>
2019-03-20 17:33:27 +0100

    Added BasketItemsController

---
+++ app/controllers/basket_items_controller.php
@@ -0,0 +1,43 @@
+<?php
+class BasketItemsController extends ApplicationController {
+
+	function edit(){
+		if(!$this->request->post()){
+			return $this->_execute_action("error404");
+		}
+
+		$product = $this->basket_item->getProduct();
+		$this->form->add_field("amount", new OrderQuantityField($product));
+
+		if($d = $this->form->validate($this->params)){
+			$this->basket_item->s($d);
+		}
+
+		$this->_redirect_to("baskets/edit");
+	}
+
+	function destroy(){
+		if(!$this->request->post()){
+			return $this->_execute_action("error404");
+		}
+
+		$this->basket_item->destroy();
+		
+		$this->_redirect_to("baskets/edit");
+	}
+
+	function _before_filter(){
+		$bi = $this->_just_find("basket_item");
+		if(!$bi){
+			$this->_execute_action("error404");
+			return;
+		}
+		$basket = $this->_get_basket();
+		if($bi->getBasketId()!==$basket->getId()){
+			$this->_execute_action("error403");
+			return;
+		}
+
+		$this->basket_item = $bi;
+	}
+}
