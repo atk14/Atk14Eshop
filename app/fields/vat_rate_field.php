4761ca4e7520b093149d4b4cbf6695c8367faf51
Jaromir Tomek <jaromir.tomek@gmail.com>
2019-10-03 15:05:43 +0200

    VatRateField fixed

--- app/fields/vat_rate_field.php
+++ app/fields/vat_rate_field.php
@@ -2,7 +2,10 @@
 class VatRateField extends ObjectChoiceField {
 
 	function __construct($options = []){
-		$options["value_formatter"] = function($vat_rate){ return sprintf('%s (%s%s)',$vat_rate->getName(),$vat_rate->getVatPercent(),"%"); };
+		$options += [
+			"order_by" => "rank, id",
+			"value_formatter" => function($vat_rate){ return sprintf('%s (%s%s)',$vat_rate->getName(),$vat_rate->getVatPercent(),"%"); },
+		];
 
 		parent::__construct($options);
 	}
