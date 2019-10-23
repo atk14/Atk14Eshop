08320dcad4db9e4487305c5f81b34fe01393ecb0
Jaromir Tomek <jaromir.tomek@gmail.com>
2019-10-23 13:52:13 +0200

    Migration fixed

--- db/migrations/0129_creating_eshop_store_migration.php
+++ db/migrations/0129_creating_eshop_store_migration.php
@@ -6,11 +6,8 @@ class CreatingEshopStoreMigration extends ApplicationMigration {
 			"code" => "eshop",
 			"visible" => false,
 			
-			"name_en" => "ATK14 Eshop",
-			"name_cs" => "ATK14 Eshop",
-
-			"name_en" => "ATK14 Eshop is an skeleton suitable for eshops. It is built on top of ATK14 Catalog.",
-			"name_cs" => "ATK14 E-shop je aplikační kostra vhodná pro e-shopy, která je postavena na ATK14 Katalogu.",
+			"name_en" => "Eshop",
+			"name_cs" => "E-shop",
 
 			"email" => "your@email.com",
 			"phone" => "+420.605123456",
