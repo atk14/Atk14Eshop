389120cb22877110b74ba50936c83337475949b4
Jaromir Tomek <jaromir.tomek@gmail.com>
2019-10-30 15:49:06 +0100

    Test fixed

--- test/controllers/initialize.php
+++ test/controllers/initialize.php
@@ -4,3 +4,6 @@ define("ATK14_DEFAULT_LANG","en");
 define("MY_BLOWFISH_ROUNDS",6); // default value 12 makes hashing very slow
 
 require(__DIR__."/../../atk14/load.php");
+
+$region = Region::GetDefaultRegion();
+$region->s("languages",'["en","cs"]');
