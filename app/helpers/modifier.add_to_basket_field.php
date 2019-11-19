e1e00dbd75537cf90d2e44aba38efab0285226f3
Jaromir Tomek <jaromir.tomek@gmail.com>
2019-11-19 13:37:45 +0100

    Helper add_to_basket_field fixed

--- app/helpers/modifier.add_to_basket_field.php
+++ app/helpers/modifier.add_to_basket_field.php
@@ -9,6 +9,7 @@
  */
 function smarty_modifier_add_to_basket_field($product){
 	$form = new ApplicationForm();
-	$form->add_field("amount", new OrderQuantityField($product));
+	$field = $form->add_field("amount", new OrderQuantityField($product));
+	$field->widget->attrs["id"] = "id_amount_".$product->getId(); // we must have unique ids on one page when more product variants are displayed
 	return $form->get_field("amount")->as_widget();
 }
