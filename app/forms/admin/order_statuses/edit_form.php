9943ab0e2e8537dca2451f4299ebd507bd7cda4a
Jaromir Tomek <jaromir.tomek@gmail.com>
2020-02-05 11:08:15 +0100

    Fix

--- app/forms/admin/order_statuses/edit_form.php
+++ app/forms/admin/order_statuses/edit_form.php
@@ -6,10 +6,11 @@ class EditForm extends OrderStatusesForm {
 
 		$this->fields["code"]->disabled = true;
 
-		if($this->controller->order_status->notificationEnabled()){
+		if($this->controller->order_status->notificationEnabled(false)){
 			$this->add_field("custom_notification_enabled", new BooleanField([
 				"label" => _("Enable notification"),
 				"required" => false,
+				"disabled" => $this->controller->order_status->getCode()=="new", // this is not good idea to disable notification for new order creation
 			]));
 			$this->add_field("bcc_email", new CharField([
 				"label" => _("BCC e-mail"),
