2621991f258bbc113a3185d527be260e299439cd
Jaromir Tomek <jaromir.tomek@gmail.com>
2020-02-11 17:59:04 +0100

    In administration, added link for creating new role for creators

--- app/controllers/admin/card_creators_controller.php
+++ app/controllers/admin/card_creators_controller.php
@@ -3,7 +3,9 @@ class CardCreatorsController extends AdminController {
 
 	function create_new(){
 		$card = $this->card;
+		$this->_add_card_to_breadcrumbs($card);
 		$this->_create_new(array(
+			"page_title" => sprintf(_('Adding creator to the product "%s"'),$this->card->getName()),
 			"create_closure" => function($d) use($card){
 				$d["card_id"] = $card;
 				return CardCreator::CreateNewRecord($d);
@@ -14,7 +16,9 @@ class CardCreatorsController extends AdminController {
 	function edit(){
 		$this->_add_card_to_breadcrumbs($this->card_creator->getCard());
 
-		$this->_edit();
+		$this->_edit(array(
+			"page_title" => _("Editing link between creator and product"),
+		));
 	}
 
 	function set_rank(){
--- app/forms/admin/card_creators/card_creators_form.php
+++ app/forms/admin/card_creators/card_creators_form.php
@@ -8,6 +8,7 @@ class CardCreatorsForm extends AdminForm {
 		]));
 		$this->add_field("creator_role_id", new CreatorRoleField([
 			"label" => _("Role"),
+			"help_text" => sprintf(_('Missing a role? &rarr; <a href="%s">create it</a>'),Atk14Url::BuildLink(["action" => "creator_roles/create_new"])),
 		]));
 		$this->add_field("is_main_creator", new BooleanField([
 			"label" => _("Is this the main creator?"),
