56a85c20d11d1c05832b04b46941abd5bb3b0c2d
Jaromir Tomek <jaromir.tomek@gmail.com>
2020-04-15 16:36:38 +0200

    Test fixed

--- test/helpers/tc_display_stockount.php
+++ test/helpers/tc_display_stockount.php
@@ -3,6 +3,7 @@
  *
  * @fixture products
  * @fixture warehouse_items
+ * @fixture pricelist_items
  */
 class TcDisplayStockount extends TcBase {
 
@@ -11,7 +12,7 @@ class TcDisplayStockount extends TcBase {
 
 		$smarty = Atk14Utils::GetSmarty();
 
-		$this->assertEquals('<span class="text-success">In stock &gt; 10 pcs</span>',smarty_function_display_stockcount(array("product" => $this->products["green_tea"]),$smarty));
+		$this->assertEquals('<span class="text-success">In stock &gt; 10 pcs</span>',smarty_function_display_stockcount(array("product" => $this->products["mint_tea"]),$smarty));
 		$this->assertEquals('<span class="text-success">In stock 5 pcs</span>',smarty_function_display_stockcount(array("product" => $this->products["black_tea"]),$smarty));
 	}
 }
