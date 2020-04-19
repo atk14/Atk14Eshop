443fc5d70aa0f80829b3894c8ff19e36cfe32a27
Jaromir Tomek <jaromir.tomek@gmail.com>
2020-04-19 22:20:06 +0200

    TraitCodebook tested

---
+++ test/models/tc_trait_codebook.php
@@ -0,0 +1,19 @@
+<?php
+class TcTraitCodebook extends TcBase {
+
+	function test(){
+		$cnt_1 = $this->dbmole->getQueriesExecuted();
+
+		SystemParameterType::GetInstanceByCode("test");
+
+		$cnt_2 = $this->dbmole->getQueriesExecuted();
+
+		$this->assertNull(SystemParameterType::GetInstanceByCode("nonexisting"));
+		$this->assertNotNull(SystemParameterType::GetInstanceById(1));
+
+		$cnt_3 = $this->dbmole->getQueriesExecuted();
+
+		$this->assertTrue($cnt_2 > $cnt_1);
+		$this->assertEquals($cnt_2,$cnt_3);
+	}
+}
