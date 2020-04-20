1038e20e74e20606e5a82a7726d80d3034d5e551
Jaromir Tomek <jaromir.tomek@gmail.com>
2020-04-20 09:45:35 +0200

    TraitCodebook tested

--- test/models/tc_trait_codebook.php
+++ test/models/tc_trait_codebook.php
@@ -1,19 +1,36 @@
 <?php
+/**
+ *
+ * @fixture delivery_methods
+ */
 class TcTraitCodebook extends TcBase {
 
 	function test(){
 		$cnt_1 = $this->dbmole->getQueriesExecuted();
 
-		SystemParameterType::GetInstanceByCode("test");
+		$dpd = DeliveryMethod::GetInstanceByCode("dpd_test");
+		$this->assertEquals("dpd_test",$dpd->getCode());
+		$dpd = DeliveryMethod::FindByCode("dpd_test");
+		$this->assertEquals("dpd_test",$dpd->getCode());
+		$this->assertNull(DeliveryMethod::FindByCode("nonexistent"));
+		$dpd = DeliveryMethod::GetInstanceById($this->delivery_methods["dpd"]->getId());
+		$this->assertEquals("dpd_test",$dpd->getCode());
+		$this->assertNull(DeliveryMethod::GetInstanceById(-123));
 
 		$cnt_2 = $this->dbmole->getQueriesExecuted();
 
-		$this->assertNull(SystemParameterType::GetInstanceByCode("nonexisting"));
-		$this->assertNotNull(SystemParameterType::GetInstanceById(1));
+		$dpd = DeliveryMethod::GetInstanceByCode("dpd_test");
+		$this->assertEquals("dpd_test",$dpd->getCode());
+		$dpd = DeliveryMethod::FindByCode("dpd_test");
+		$this->assertEquals("dpd_test",$dpd->getCode());
+		$this->assertNull(DeliveryMethod::FindByCode("nonexistent"));
+		$dpd = DeliveryMethod::GetInstanceById($this->delivery_methods["dpd"]->getId());
+		$this->assertEquals("dpd_test",$dpd->getCode());
+		$this->assertNull(DeliveryMethod::GetInstanceById(-123));
 
 		$cnt_3 = $this->dbmole->getQueriesExecuted();
 
-		$this->assertEquals($cnt_2,$cnt_1 + 2);
+		$this->assertEquals($cnt_2,$cnt_1+2);
 		$this->assertEquals($cnt_2,$cnt_3);
 	}
 }
