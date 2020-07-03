bba16f46f8d1f69b977041609d4f89de03403f4b
Jaromir Tomek <jaromir.tomek@gmail.com>
2020-07-03 13:16:50 +0200

    Test fixed

--- test/helpers/tc_color_to_rgba.php
+++ test/helpers/tc_color_to_rgba.php
@@ -5,18 +5,18 @@ class TcColorToRgba extends TcBase {
 		Atk14Require::Helper("modifier.color_to_rgba");
 
 		$this->assertEquals("rgba(0,17,255,0.5)",smarty_modifier_color_to_rgba("#0011ff","0.5"));
-		$this->assertEquals("rgba(0,17,255,1.0)",smarty_modifier_color_to_rgba("#0011ff")); // 1 is default
+		$this->assertEquals("rgba(0,17,255,1)",smarty_modifier_color_to_rgba("#0011ff")); // 1 is default
 
-		$this->assertEquals("rgba(0,17,255,1.0)",smarty_modifier_color_to_rgba("#0011FFAA")); // hexa
+		$this->assertEquals("rgba(0,17,255,1)",smarty_modifier_color_to_rgba("#0011FFAA")); // hexa
 
 		$this->assertEquals("rgba(170,187,204,0.5)",smarty_modifier_color_to_rgba("#abc","0.5"));
 
 		$this->assertEquals("rgba(1,2,3,0.5)",smarty_modifier_color_to_rgba("rgb(1,2,3)","0.5"));
 
-		$this->assertEquals("rgba(33,44,55,1.0)",smarty_modifier_color_to_rgba("rgba(33,44,55,0.3)"));
-		$this->assertEquals("rgba(33.3,44.4,55.5,0.5)",smarty_modifier_color_to_rgba("rgba(33.3, 44.4, 55.5, 0.3)","0.5"));
+		$this->assertEquals("rgba(33,44,55,1)",smarty_modifier_color_to_rgba("rgba(33,44,55,0.3)"));
+		$this->assertEquals("rgba(33,44,55,0.5)",smarty_modifier_color_to_rgba("rgba(33.3, 44.4, 55.5, 0.3)","0.5"));
 
 		$this->assertEquals("",smarty_modifier_color_to_rgba("","0.5"));
-		$this->assertEquals("",smarty_modifier_color_to_rgba("nonsence","0.5"));
+		$this->assertEquals("rgba(255,255,255,0.5)",smarty_modifier_color_to_rgba("nonsence","0.5")); // fallback
 	}
 }
