4b93d761ae7e8e27757cefb9b79b86017c202bab
Jaromir Tomek <jaromir.tomek@gmail.com>
2020-07-03 13:57:52 +0200

    Testing

--- test/helpers/tc_color_to_rgba.php
+++ test/helpers/tc_color_to_rgba.php
@@ -16,6 +16,10 @@ class TcColorToRgba extends TcBase {
 		$this->assertEquals("rgba(33,44,55,1)",smarty_modifier_color_to_rgba("rgba(33,44,55,0.3)"));
 		$this->assertEquals("rgba(33,44,55,0.5)",smarty_modifier_color_to_rgba("rgba(33.3, 44.4, 55.5, 0.3)","0.5"));
 
+		// named colors - see vendor/aristath/ari-color/aricolor.php
+		$this->assertEquals("rgba(255,0,0,1)",smarty_modifier_color_to_rgba("RED"));
+		$this->assertEquals("rgba(238,130,238,1)",smarty_modifier_color_to_rgba("violet"));
+
 		$this->assertEquals("",smarty_modifier_color_to_rgba("","0.5"));
 		$this->assertEquals("rgba(255,255,255,0.5)",smarty_modifier_color_to_rgba("nonsence","0.5")); // fallback
 	}
