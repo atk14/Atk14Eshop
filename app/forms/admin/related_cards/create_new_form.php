0c298f61eeb5346c96cf952cc50fae7cb6fc48a0
Jaromir Tomek <jaromir.tomek@gmail.com>
2020-07-14 15:32:21 +0200

    An opposite link can be created during new related card addition

--- app/forms/admin/related_cards/create_new_form.php
+++ app/forms/admin/related_cards/create_new_form.php
@@ -1,8 +1,15 @@
 <?php
 class CreateNewForm extends AdminForm {
+
 	function set_up() {
 		$this->add_field("adding_card", new CardField(array(
 			"label" => _("Product")
+		)));
+
+		$this->add_field("also_create_opposite_link", new BooleanField(array(
+			"label" => _("Vytvořit také opačnou vazbu?"),
+			"required" => false,
+			"initial" => true,
 		)));
 
 		$this->set_button_text(_("Add product"));
