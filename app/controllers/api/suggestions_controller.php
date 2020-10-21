cf08a142fbb9af2ca2824ba9bf3d9e128d1601b8
Jaromir Tomek <jaromir.tomek@gmail.com>
2020-10-21 13:43:42 +0200

    Fix

--- app/controllers/api/suggestions_controller.php
+++ app/controllers/api/suggestions_controller.php
@@ -12,12 +12,19 @@ class SuggestionsController extends ApiController{
 			"fields" => [
 				"login",
 				"firstname",
-				"lastname"
+				"lastname",
+				"company",
+				"address_street",
+				"address_street2",
+				"address_city",
+				"address_state",
+				"address_zip"
 			],
 			"order_by" => "
 				LOWER(login) LIKE LOWER(:q)||'%' DESC,
 				LOWER(lastname) LIKE LOWER(:q)||'%' DESC,
 				LOWER(firstname) LIKE LOWER(:q)||'%' DESC,
+				LOWER(company) LIKE LOWER(:q)||'%' DESC,
 				login
 			",
 		]);
