5644382c32723f6d3ac8ce5d42df3730b50cecdf
Jaromir Tomek <jaromir.tomek@gmail.com>
2021-03-28 15:53:03 +0200

    Migration fixed

--- db/migrations/0110_zz01_updating_price_rounding_product.sql
+++ db/migrations/0110_zz01_updating_price_rounding_product.sql
@@ -1,1 +1,1 @@
-UPDATE products SET vat_rate_id=(SELECT id FROM vat_rates WHERE code='default') WHERE code='price_rounding';
+UPDATE products SET vat_rate_id=(SELECT id FROM vat_rates WHERE code='default') WHERE code='price_rounding' AND vat_rate_id IS NULL;
