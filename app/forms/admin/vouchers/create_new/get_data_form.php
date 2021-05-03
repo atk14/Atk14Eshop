60b165fa4aa426e3373030806e15290315b6a285
Jaromir Tomek <jaromir.tomek@gmail.com>
2021-05-03 10:09:04 +0200

    Fix

--- app/forms/admin/vouchers/create_new/get_data_form.php
+++ app/forms/admin/vouchers/create_new/get_data_form.php
@@ -1,4 +1,13 @@
 <?php
 class GetDataForm extends VouchersForm {
 
+	function clean(){
+		list($err,$d) = parent::clean();
+
+		if(isset($d["voucher_code"]) && Voucher::FindFirst("voucher_code",$d["voucher_code"])){
+			$this->set_error("voucher_code",_("Stejný kód má jiný slevový kupón"));
+		}
+
+		return [$err,$d];
+	}
 }
