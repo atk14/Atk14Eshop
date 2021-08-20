51a4d74c51e7128b63c78ea1a071706f736fb093
Jaromir Tomek <jaromir.tomek@gmail.com>
2021-08-21 00:36:15 +0200

    Administration of regions improved

--- app/forms/admin/regions/regions_form.php
+++ app/forms/admin/regions/regions_form.php
@@ -7,6 +7,15 @@ class RegionsForm extends AdminForm {
 			"max_length" => 255,
 		]));
 
+		$this->add_field("currencies", new CurrenciesField([
+			"label" => _("Supported currencies"),
+			"json_encode" => true,
+			"required" => true,
+			"max_choice_items" => 1,
+		]))->update_messages([
+			"max_choice_items_1" => _("Currently, only one currency can be selected.")
+		]);
+
 		$this->add_code_field([
 			"required" => true,
 			"disabled" => true,
