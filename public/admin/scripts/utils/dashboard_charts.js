1f6b564b84422569e8fd72c7104b3a97ada4e2d5
Jakub Tylčer <tylcer@snapps.eu>
2021-12-03 12:48:39 +0100

    chartjs3 in progress

--- public/admin/scripts/utils/dashboard_charts.js
+++ public/admin/scripts/utils/dashboard_charts.js
@@ -4,8 +4,9 @@ window.UTILS.initDashboardOrdersChart = function() {
 
 	// eslint-disable-next-line no-undef
 	//Chart.plugins.unregister(ChartDataLabels);
-	Chart.unregister(ChartDataLabels);
-	Chart.register(ChartDataLabels);
+	//Chart.unregister(ChartDataLabels);
+	// eslint-disable-next-line no-undef
+	////Chart.register(ChartDataLabels);
 
 	// eslint-disable-next-line no-undef
 	var color = Chart.helpers.color;
@@ -14,7 +15,7 @@ window.UTILS.initDashboardOrdersChart = function() {
 	var currentResolution = "days";
 	var pageOffset = 0;
 
-	var ordersChartConfig = {
+	var XordersChartConfig = {
 		data: {
 			datasets: [{
 				label: "",
@@ -173,6 +174,41 @@ window.UTILS.initDashboardOrdersChart = function() {
 		}
 	}
 	
+	var ordersChartConfig = {
+    type: 'bar',
+    data: {
+        labels: ['Red', 'Blue', 'Yellow', 'Green', 'Purple', 'Orange'],
+        datasets: [{
+            label: '# of Votes',
+            data: [12, 19, 3, 5, 2, 3],
+            backgroundColor: [
+                'rgba(255, 99, 132, 0.2)',
+                'rgba(54, 162, 235, 0.2)',
+                'rgba(255, 206, 86, 0.2)',
+                'rgba(75, 192, 192, 0.2)',
+                'rgba(153, 102, 255, 0.2)',
+                'rgba(255, 159, 64, 0.2)'
+            ],
+            borderColor: [
+                'rgba(255, 99, 132, 1)',
+                'rgba(54, 162, 235, 1)',
+                'rgba(255, 206, 86, 1)',
+                'rgba(75, 192, 192, 1)',
+                'rgba(153, 102, 255, 1)',
+                'rgba(255, 159, 64, 1)'
+            ],
+            borderWidth: 1
+        }]
+    },
+    options: {
+        scales: {
+            y: {
+                beginAtZero: true
+            }
+        }
+    }
+}
+
 	// eslint-disable-next-line no-undef
 	var ordersChart = new Chart( ordersChartCtx, ordersChartConfig );
 	toggleResolution( "days" )
