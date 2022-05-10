ae9a788c694d53d7442148c92408631e69611bfe
Jakub Tylčer <tylcer@snapps.eu>
2022-05-10 14:04:26 +0200

    styling photoswipe

--- public/scripts/modules/application_es6.js
+++ public/scripts/modules/application_es6.js
@@ -6,4 +6,38 @@ const lightbox = new PhotoSwipeLightbox({
   pswpModule: () => import("/public/dist/scripts/modules/photoswipe.esm.min.js")
 });
 console.log(lightbox);
+
+lightbox.on('uiRegister', function() {
+  console.log( "uiRegister" );
+  lightbox.pswp.ui.registerElement({
+    name: 'custom-caption',
+    order: 9,
+    isButton: false,
+    appendTo: 'root',
+    html: '',
+    onInit: (el, pswp) => {
+      console.log( "onInit" );
+      lightbox.pswp.on('change', () => {
+        console.log( "change" );
+        const currSlideElement = lightbox.pswp.currSlide.data.element;
+        const parentFigure = currSlideElement.closest( "figure" );
+        const figcaption = parentFigure.querySelector( "figcaption" );
+        console.log( currSlideElement.closest( "figure" ).querySelector( "figcaption" ) );
+        let captionHTML = '';
+        if (currSlideElement) {
+          const hiddenCaption = currSlideElement.querySelector('.hidden-caption-content');
+          // get caption from alt attribute
+          // captionHTML = currSlideElement.querySelector('img').getAttribute('alt');
+          // Get caption from figcaption tag
+          console.log( "figcation", figcaption );
+          if( figcaption ) {
+            captionHTML = figcaption.innerHTML;
+          }
+        }
+        el.innerHTML = captionHTML || '';
+      });
+    }
+  });
+});
+
 lightbox.init();
