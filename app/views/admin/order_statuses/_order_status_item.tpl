d3749e9a0593484e85dbb74ea3def3ae5638f50e
Jaromir Tomek <jaromir.tomek@gmail.com>
2022-06-21 10:00:34 +0200

    [Next automatic order status] Next automatic order status and days to automatic update can be set on an order status

---
+++ app/views/admin/order_statuses/_order_status_item.tpl
@@ -0,0 +1,35 @@
+{assign next_automatic_order_status $order_status->getNextAutomaticOrderStatus()}
+
+<li class="list-group-item">
+	<div class="item__properties">
+
+	<div class="item__title">
+		<strong>{render partial="shared/order_status" order_status=$order_status}</strong>
+		<small>
+			<ul class="list-unstyled">
+				<li>{t}code{/t}: {$order_status->getCode()}</li>
+				{if $order_status->notificationEnabled(false)}
+					<li>{if $order_status->notificationEnabled()}{t}it is notified{/t}{else}{t}notification disabled{/t}{/if}</li>
+					<li>{t}bcc{/t}: {$order_status->getBccEmail()|default:$mdash}</li>
+				{/if}
+			</ul>
+		</small>
+	</div>
+
+	<div class="item__properties">
+		{if $next_automatic_order_status}
+			<small>
+				{t days=$order_status->getNextAutomaticOrderStatusAfterDays()}After %1 days it will automatically go into the state:{/t}<br>
+				{render partial="shared/order_status" order_status=$next_automatic_order_status}
+			</small>
+		{/if}
+	</div>
+
+	<div class="item__controls">
+		{dropdown_menu}
+			{a action="edit" id=$order_status}{!"edit"|icon} {t}Upravit{/t}{/a}
+		{/dropdown_menu}
+	</div>
+
+	</div>
+</li>
--- robots/order_status_updater_robot.php
+++ robots/order_status_updater_robot.php
@@ -5,35 +5,33 @@ class OrderStatusUpdaterRobot extends ApplicationRobot {
 		$robot = User::FindById(User::ID_ROBOT);
 		$now = now();
 		$created_at_min = Date::ByDate($now)->minusDays(30)->toString();
+		$order_statuses = OrderStatus::FindAll("next_automatic_order_status_id IS NOT NULL");
 
-		$codes = [
-			"repeated_payment_request",
-			"repeated_call_for_pickup_order",
-		];
-		$cancelled = OrderStatus::GetInstanceByCode("cancelled");
-
-		// repeated_payment_request or repeated_call_for_pickup_order -> after ~ 10 days -> cancelled
-		$orders = Order::FindAll([
-			"conditions" => [
-				"created_at>:created_at_min",
-				"order_status_id IN (SELECT id FROM order_statuses WHERE code IN :codes)",
-				"DATE_TRUNC('day',order_status_set_at) + INTERVAL '11 days'<=:now"
-			],
-			"bind_ar" => [
-				":created_at_min" => $created_at_min,
-				":codes" => $codes,
-				":now" => now(),
-			]
-		]);
-		foreach($orders as $order){
-			$this->dbmole->begin();
-			$current_order_status = $order->getOrderStatus()->getCode();
-			$order->setNewOrderStatus([
-				"order_status_id" => $cancelled,
-				"order_status_set_by_user_id" => $robot,
+		foreach($order_statuses as $order_status){	
+			$orders = Order::FindAll([
+				"conditions" => [
+					"created_at>:created_at_min",
+					"order_status_id=:order_status",
+					"DATE_TRUNC('day',order_status_set_at) + INTERVAL :days||' days'<=:now"
+				],
+				"bind_ar" => [
+					":created_at_min" => $created_at_min,
+					":order_status" => $order_status,
+					":days" => (string)$order_status->getNextAutomaticOrderStatusAfterDays(),
+					":now" => now(),
+				]
 			]);
-			$this->dbmole->commit();
-			$this->logger->info(sprintf("order status updated on order %s (Order#%s) from $current_order_status to cancelled",$order->getOrderNo(),$order->getId()));
+			foreach($orders as $order){
+				$this->dbmole->begin();
+				$current_order_status = $order->getOrderStatus();
+				$next_automatic_order_status = $order_status->getNextAutomaticOrderStatus();
+				$order->setNewOrderStatus([
+					"order_status_id" => $next_automatic_order_status,
+					"order_status_set_by_user_id" => $robot,
+				]);
+				$this->dbmole->commit();
+				$this->logger->info(sprintf("order status automatically updated on order %s (Order#%s) from %s to %s",$order->getOrderNo(),$order->getId(),$current_order_status->getCode(),$next_automatic_order_status->getCode()));
+			}
 		}
 	}
 }
