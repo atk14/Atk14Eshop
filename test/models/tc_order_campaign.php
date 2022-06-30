e128cff7c878421154dd84dc151236892d7681a6
Jaromir Tomek <jaromir.tomek@gmail.com>
2022-06-30 18:13:04 +0200

    [Gift to order] Testing

--- test/models/tc_order_campaign.php
+++ test/models/tc_order_campaign.php
@@ -36,4 +36,33 @@ class TcOrderCampaign extends TcBase {
 		$this->assertEquals(58.73,$campaigns[0]->getDiscountAmount(false)); // used averaged items VAT percent
 		$this->assertEquals(null,$campaigns[0]->getVatPercent());
 	}
+
+	function test_gift(){
+		$campaign_gift = Campaign::CreateNewRecord([
+			"minimal_items_price_incl_vat" => 2000,
+			"regions" => '{"CR": true, "SK": true, "EU": true}',
+			"gift_product_id" => $this->products["fidorka"],
+			"gift_amount" => 1,
+			"gift_multiply" => true,
+		]);
+
+		$basket = $this->_prepareEmptyBasket();
+		$basket->setProductAmount($this->products["fridge"],1); // 11999 CZK inc. VAT
+
+		$order = $basket->createOrder();
+		$campaigns = $order->getCampaigns();
+
+		$items = $order->getItems();
+		$this->assertEquals(2,sizeof($items));
+
+		$this->assertEquals($this->products["fridge"]->getId(),$items[0]->getProductId());
+		$this->assertEquals(1,$items[0]->getAmount());
+
+		$this->assertEquals($this->products["fidorka"]->getId(),$items[1]->getProductId());
+		$this->assertEquals(5,$items[1]->getAmount());
+
+		$this->assertEquals(1,sizeof($campaigns));
+
+		$this->assertEquals($campaigns[0]->getGiftOrderItem()->getId(),$items[1]->getId());
+	}
 }
