376adf6cea119bb09b930236cc78f60065e2ce13
Jaromir Tomek <jaromir.tomek@gmail.com>
2022-10-22 23:53:46 +0200

    Fix

--- app/controllers/categories_controller.php
+++ app/controllers/categories_controller.php
@@ -18,6 +18,8 @@ class CategoriesController extends CardListController{
 
 	function detail(){
 		$this->_detail();
-		$this->head_tags->setCanonical($this->_link_to(["path" => $this->tpl_data["category"]->getPath()], ["with_hostname" => true]));
+		if($this->tpl_data["category"]){
+			$this->head_tags->setCanonical($this->_link_to(["path" => $this->tpl_data["category"]->getPath()], ["with_hostname" => true]));
+		}
 	}
 }
