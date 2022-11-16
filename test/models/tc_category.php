60634843cd20a0389ced48d03f29b166d58fc556
Jaromir Tomek <jaromir.tomek@gmail.com>
2022-11-16 13:45:20 +0100

    Testing

--- test/models/tc_category.php
+++ test/models/tc_category.php
@@ -3,6 +3,8 @@
  *
  * @fixture categories
  * @fixture cards
+ * @fixture tags
+ * @fixture category_tags
  */
 class TcCategory extends TcBase {
 
@@ -414,5 +416,22 @@ class TcCategory extends TcBase {
 
 		$shoes = $this->categories["shoes"];
 		$this->assertFalse($shoes->isMainRootCategory());
+	}
+
+	function test_containsTag(){
+		$sale = $this->tags["sale"];
+		$food_drinks = $this->categories["food_drinks"];
+		$hot_drinks = $this->categories["hot_drinks"];
+
+		$this->assertTrue($food_drinks->containsTag($sale));
+		$this->assertFalse($hot_drinks->containsTag($sale));
+		$this->assertFalse($hot_drinks->containsTag($sale,["consider_parents" => false]));
+		$this->assertTrue($hot_drinks->containsTag($sale,["consider_parents" => true]));
+
+		// alias hasTag
+		$this->assertTrue($food_drinks->hasTag($sale));
+		$this->assertFalse($hot_drinks->hasTag($sale));
+		$this->assertFalse($hot_drinks->hasTag($sale,["consider_parents" => false]));
+		$this->assertTrue($hot_drinks->hasTag($sale,["consider_parents" => true]));
 	}
 }
