1d9a2369675c8a5638cac1fa5640b9fbf28cf934
Jaromir Tomek <jaromir.tomek@gmail.com>
2023-02-24 17:57:42 +0100

    FavouriteProductsAccessor optimized

--- app/models/favourite_products_accessor.php
+++ app/models/favourite_products_accessor.php
@@ -13,6 +13,8 @@ class FavouriteProductsAccessor {
 	protected $user;
 	protected $session;
 
+	protected $CACHED_AR;
+
 	function __construct($user,$session = null){
 		if(!$user){
 			$user = User::GetAnonymousUser();
@@ -27,9 +29,17 @@ class FavouriteProductsAccessor {
 	 * @return FavouriteProduct[]
 	 */
 	function getFavouriteProducts(){
-		$session_salt = $this->_getSessionSalt();
-		if(is_null($session_salt)){ return []; }
-		return FavouriteProduct::FindAll("user_id",$this->user,"session_salt",$session_salt);
+		$this->_prepareCahedAr();
+		return Cache::Get("FavouriteProduct",array_values($this->CACHED_AR));
+	}
+
+	/**
+	 *
+	 * @return int
+	 */
+	function getFavouriteProductsCount(){
+		$this->_prepareCahedAr();
+		return sizeof($this->CACHED_AR);
 	}
 
 	function mergeFavouriteProductsAccessor($favourite_products_accessor){
@@ -42,6 +52,7 @@ class FavouriteProductsAccessor {
 		foreach($this->getFavouriteProducts() as $fp){
 			$fp->destroy();
 		}
+		$this->CACHED_AR = [];
 	}
 
 	function addProduct($product){
@@ -52,21 +63,41 @@ class FavouriteProductsAccessor {
 				"session_salt" => $session_salt,
 				"product_id" => $product,
 			]);
+			$p_id = TableRecord::ObjToId($product);
+
+			// associative unshift
+			$keys = array_keys($this->CACHED_AR);
+			$values = array_values($this->CACHED_AR);
+			array_unshift($keys,$p_id);
+			array_unshift($values,$fp->getId());
+			$this->CACHED_AR = array_combine($keys,$values);
+
+			//$this->CACHED_AR[$p_id] = $fp->getId();
 		}
 		return $fp;
 	}
 
 	function delProduct($product){
 		$fp = $this->isFavouriteProduct($product,true);
-		if($fp){ $fp->destroy(); }
+		if($fp){
+			$p_id = TableRecord::ObjToId($product);
+			unset($this->CACHED_AR[$p_id]);
+			$fp->destroy();
+		}
 	}
 
 	function isFavouriteProduct($product,$return_favourite_product = false){
-		$session_salt = $this->_getSessionSalt();
-		if(is_null($session_salt)){ return false; }
-		$fp = FavouriteProduct::FindFirst("user_id",$this->user,"session_salt",$session_salt,"product_id",$product);
+		$this->_prepareCahedAr();
 
-		return $return_favourite_product ? $fp : !!$fp;
+		$p_id = TableRecord::ObjToId($product);
+		if($return_favourite_product){
+			if(isset($this->CACHED_AR[$p_id])){
+				return Cache::Get("FavouriteProduct",$this->CACHED_AR[$p_id]);
+			}
+			return null;
+		}
+
+		return isset($this->CACHED_AR[$p_id]);
 	}
 
 	function isFavouriteCard($card){
@@ -99,5 +130,28 @@ class FavouriteProductsAccessor {
 			$this->session->s("favourite_products_salt",$session_salt);
 		}
 		return $session_salt;
+	}
+
+	protected function _prepareCahedAr(){
+		if(!is_null($this->CACHED_AR)){ return; }
+
+		$this->CACHED_AR = [];
+		$session_salt = $this->_getSessionSalt(false);
+		if(is_null($session_salt)){
+			return;
+		}
+
+		$conditions = $bind_ar = [];
+		$conditions[] = "session_salt=:session_salt";
+		$bind_ar[":session_salt"] = $session_salt;
+		if($this->user){
+			$conditions[] = "user_id=:user";
+			$bind_ar[":user"] = $this->user;
+		}else{
+			$conditions[] = "user_id IS NULL";
+		}
+		
+		$dbmole = FavouriteProduct::GetDbmole();
+		$this->CACHED_AR = $dbmole->selectIntoAssociativeArray("SELECT product_id,id FROM favourite_products WHERE ".join(" AND ",$conditions)." ORDER BY created_at DESC, id DESC",$bind_ar);
 	}
 }
