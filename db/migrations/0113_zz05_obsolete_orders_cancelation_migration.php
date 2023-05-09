81a7ca0a3b62cd4c8690076369da832f8add504f
Jaromir Tomek <jaromir.tomek@gmail.com>
2023-05-09 12:42:11 +0200

    Migration fixed

--- db/migrations/0113_zz05_obsolete_orders_cancelation_migration.php
+++ db/migrations/0113_zz05_obsolete_orders_cancelation_migration.php
@@ -8,7 +8,7 @@ class Zz05ObsoleteOrdersCancelationMigration extends ApplicationMigration {
 		$order_statuses = OrderStatus::FindAll("next_automatic_order_status_id IS NOT NULL");
 
 		foreach($order_statuses as $order_status){
-			foreach(Order::FindAll("order_status_id=:order_status AND order_status_set_at<NOW() - INTERVAL '20 days'",[":order_status" => $order_status,]) as $order){
+			foreach(Order::FindAll("order_status_id=:order_status AND order_status_set_at<NOW() - INTERVAL '20 days'",[":order_status" => $order_status],["order_by" => "order_status_set_at"]) as $order){
 				$this->logger->info(sprintf("Status updated on Order#%s, order_no=%s: %s -> cancelled",$order->getId(),$order->getOrderNo(),$order->getOrderStatus()->getCode()));
 				$order->setNewOrderStatus([
 					"order_status_id" => $cancelled,
