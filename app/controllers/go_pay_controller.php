c7dc0eaf5bda875b1930b5a8b194d3f31130082b
Jaromir Tomek <jaromir.tomek@gmail.com>
2023-06-22 18:32:35 +0200

    [GoPay] Added action go_pay/update_status

--- app/controllers/go_pay_controller.php
+++ app/controllers/go_pay_controller.php
@@ -9,4 +9,26 @@ class GoPayController extends PaymentGatewaysBaseController {
 		]);
 	}
 
+	function update_status(){
+		$gateway_code = "go_pay";
+		$parameter_name = "id";
+
+		$this->render_template = false;
+
+		$gateway = PaymentGateway::GetInstanceByCode($gateway_code);
+
+		$pt = null;
+		if($this->params->defined($parameter_name)){
+			$transaction_id = $this->params->getString($parameter_name);
+			$pt = PaymentTransaction::FindFirst("payment_transaction_id",$transaction_id,"payment_gateway_id",$gateway,["order_by" => "created_at DESC, id DESC"]);
+		}
+
+		if(!$pt || $pt->getPaymentGateway()->getCode()!==$gateway_code){
+			$this->response->setStatusCode(404);
+			return;
+		}
+
+		$gateway_api = $pt->getPaymentGatewayApi();
+		$gateway_api->updateStatus($pt);
+	}
 }
