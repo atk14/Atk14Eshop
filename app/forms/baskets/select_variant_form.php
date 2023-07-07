6f4c43747bada4dcf41d85f2ad315f4968457618
Jaromir Tomek <jaromir.tomek@gmail.com>
2023-07-07 12:54:12 +0200

    Method SelectVariantForm::prepare_for_card() improved

--- app/forms/baskets/select_variant_form.php
+++ app/forms/baskets/select_variant_form.php
@@ -10,10 +10,15 @@ class SelectVariantForm extends BasketsForm {
 
 	function prepare_for_card($card){
 		$choices = ["" => "-- "._("Select variant")." --"];
+		$disabled_choices = [];
 		foreach($card->getProducts() as $product){
 			$choices[$product->getId()] = $product->getLabel();
+			if(!$product->canBeORdered()){
+				$disabled_choices[] = $product->getId();
+			}
 		}
 
 		$this->fields["product_id"]->set_choices($choices);
+		$this->fields["product_id"]->set_disabled_choices($disabled_choices);
 	}
 }
