600b0e51ecb09502a1f2d1acfba606b9deb81a47
Jaromir Tomek <jaromir.tomek@gmail.com>
2023-08-18 17:59:34 +0200

    PaymentStatusCheckerRobot updated

--- robots/payment_status_checker_robot.php
+++ robots/payment_status_checker_robot.php
@@ -8,15 +8,20 @@ class PaymentStatusCheckerRobot extends ApplicationRobot {
 		//$TESTING_PAYMENTS_ONLY = !PRODUCTION;
 		$TESTING_PAYMENTS_ONLY = false;
 
+		$order_status_id = "(SELECT order_status_id FROM orders WHERE id=payment_transactions.order_id)";
 		$conditions = [
 			"payment_transaction_started_at IS NOT NULL",
-			"payment_transaction_started_at<NOW() - INTERVAL '5 minutes'", // nekontroluji se uplne nove platebni transakce - ocekava se, ze stav zaplaceni oznami plat. brana push zpravou
-			"payment_transaction_started_at>NOW() - INTERVAL '24 hours'", // pouze 1 den se kontroluji nezaplacene platby - pak uz to je na rucnim zpracovani...
 			"payment_status_id=:pending_status OR payment_status_id IS NULL",
+			"(payment_transaction_started_at<NOW() - INTERVAL '5 minutes') OR payment_status_id=:pending_status", // nekontroluji se uplne nove platebni transakce - ocekava se, ze stav zaplaceni oznami plat. brana push zpravou
+			"
+				(payment_transaction_started_at>NOW() - INTERVAL '24 hours') OR -- 1 den se pravidelne kontroluji nezaplacene platby
+				(payment_transaction_started_at>NOW() - INTERVAL '10 days' AND $order_status_id=:payment_failed_order_status AND COALESCE(payment_status_checked_at,created_at)<NOW() - INTERVAL '30 minutes') -- a pak kazdou pulhodinu u objednavek ve stavu payment_failed
+			", // 
 		];
 
 		$bind_ar = [
 			":pending_status" => PaymentStatus::FindByCode("pending"),
+			":payment_failed_order_status" => OrderStatus::FindByCode("payment_failed"),
 		];
 
 		if($TESTING_PAYMENTS_ONLY){
