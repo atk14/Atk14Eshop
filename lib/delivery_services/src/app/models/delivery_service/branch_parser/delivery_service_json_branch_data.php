6a1cd5cf041e1200c03b8c95ad81984c08ddefe4
Jaromir Tomek <jaromir.tomek@gmail.com>
2023-11-23 12:34:49 +0100

    [pickup_points] Added method DeliveryServiceJsonBranchData::GetInstance()

--- lib/delivery_services/src/app/models/delivery_service/branch_parser/delivery_service_json_branch_data.php
+++ lib/delivery_services/src/app/models/delivery_service/branch_parser/delivery_service_json_branch_data.php
@@ -4,6 +4,17 @@ namespace DeliveryService\BranchParser;
 
 class DeliveryServiceJsonBranchData extends SimpleJsonElement {
 
+	public static function GetInstance(
+		string $data,
+		int $options = 0,
+		bool $dataIsURL = false,
+		string $namespaceOrPrefix = "",
+		bool $isPrefix = false
+	) {
+		$instance = new static($data, $options, $dataIsURL, $namespaceOrPrefix, $isPrefix);
+		return $instance;
+	}
+
 	function toArray() {
 		return [
 			"external_branch_id" => $this->getExternalBranchId(),
