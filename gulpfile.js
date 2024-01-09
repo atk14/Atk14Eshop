88e25429576256cc2b13e151ee0d8a7e7c308d13
Jakub Tylčer <tylcer@snapps.eu>
2024-01-09 13:45:26 +0100

    Removed unused jquery-ui from frontend

--- gulpfile.js
+++ gulpfile.js
@@ -17,7 +17,7 @@ var vendorStyles = [
 
 var vendorScripts = [
 	"node_modules/jquery/dist/jquery.js",
-	"node_modules/jquery-ui-bundle/jquery-ui.js",
+	//"node_modules/jquery-ui-bundle/jquery-ui.js",
 	"node_modules/bootstrap/dist/js/bootstrap.bundle.js", // Bootstrap + Popper
 	"node_modules/atk14js/src/atk14.js",
 	"node_modules/unobfuscatejs/src/jquery.unobfuscate.js",
