cb3ae541d4cdc94a5e94de5c343023c257c8effc
Jakub Tylčer <tylcer@snapps.eu>
2024-01-29 17:18:36 +0100

    image uploader in progress

--- public/admin/scripts/utils/async_image_upload.js
+++ public/admin/scripts/utils/async_image_upload.js
@@ -75,38 +75,71 @@ window.UTILS.AsyncImageUploader = class {
 
   uploadFile( file ) {
     console.log( "UPLOAD ME", file );
-    //this.uploads.push( new this.fileUpload( file, this ) );
-    let formData = new FormData();
-    formData.append( "file", file, file.name );
+
     let xhr = new XMLHttpRequest();
     this.uploads.push( xhr );
     //xhr.test = "ahoj" + file.name;
     xhr.open( "POST", this.url, true );
     xhr.responseType = "json";
-    xhr.upload.addEventListener( "progress", function ( e ) {
-      //$this.updateProgress( (e.loaded * 100.0 / e.total) || 100 );
-      console.log( "progress", (e.loaded * 100.0 / e.total) || 100 );
-    } );
 
-    xhr.addEventListener( "readystatechange", function () {
-      if ( xhr.readyState === 4 && xhr.status >= 200 && xhr.status < 400 ) {
-        //$this.onSuccess( xhr.response );
-        console.log( "complate" );
-      } else if( xhr.readyState === 4 ) {
-        //$this.onError( xhr.response );
-        console.log( "error" );
-      }
-    } );
+    xhr.upload.addEventListener( "progress", this.onUploadProgress.bind( this ) );
+
+    xhr.addEventListener( "readystatechange", this.onReadyStateChange.bind( this ) );
+    
     console.log("XHR", xhr)
-    //xhr.send( formData );
-    //xhr.setRequestHeader('X-File-Name', file.name);
-    //xhr.setRequestHeader('X-File-Size', file.size);
+    
     xhr.setRequestHeader( 'Content-Type', file.type);
     xhr.setRequestHeader( 'Accept', 'application/json, text/javascript, text/plain, */*' );
     xhr.setRequestHeader( 'Content-Disposition', 'attachment; filename=' + encodeURIComponent( file.name ) );
     xhr.setRequestHeader( 'X-Requested-With', 'XMLHttpRequest' );
     
     xhr.send(file);
+  }
+
+  onUploadProgress( e ) {
+    let loaded = 0;
+    let total = 0;
+    e.target.bytesLoaded = e.loaded;
+    e.target.bytesTotal = e.total;
+    for( let i = 0; i < this.uploads.length; i++) {
+      loaded += this.uploads[ i ].upload.bytesLoaded;
+      total += this.uploads[ i ].upload.bytesTotal;
+    }
+    this.progress.style.width = loaded * 100 / total + "%";
+  }
+
+  onReadyStateChange( e ) {
+    if ( e.target.readyState === 4 && e.target.status >= 200 && e.target.status < 400 ) {
+      //$this.onSuccess( xhr.response );
+      console.log( "complate" );
+      console.log( e.target.response.image_gallery_item );
+      console.log( e.target );
+      let glyph = "<span class='fas fa-grip-vertical text-secondary handle pr-3' " +
+      " title='sorting'></span>";
+     // let h = new DOMParser().parseFromString( e.target.response.image_gallery_item, "text/html" );
+      this.list.insertAdjacentHTML( "beforeend", e.target.response.image_gallery_item );
+      //this.list.append( e.target.response.image_gallery_item );
+      this.checkComplete();
+    } else if( e.target.readyState === 4 ) {
+      //$this.onError( xhr.response );
+      console.log( "error" );
+    }
+  }
+
+  checkComplete() {
+    console.log( "-------------------");
+    let completedUploads = 0;
+    for( let i = 0; i < this.uploads.length; i++) {
+      console.log(this.uploads[i].readyState, this.uploads[i].status );
+      if( this.uploads[i].readyState === 4 && this.uploads[i].status >= 200 && this.uploads[i].status < 400 ) {
+        completedUploads ++;
+      }
+    }
+    console.log( "-------------------", completedUploads );
+    if( this.uploads.length === completedUploads ) {
+      this.progress.style.width = "0%";
+      this.uploads = [];
+    }
   }
 
   highlight( e ) {
