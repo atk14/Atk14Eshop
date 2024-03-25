0b2621820d15f4b0c8c4042aa5141b5f39dbb63c
Jaromir Tomek <jaromir.tomek@gmail.com>
2024-03-25 17:18:46 +0100

    Testing

--- test/helpers/tc_display_stockount.php
+++ test/helpers/tc_display_stockount.php
@@ -14,5 +14,6 @@ class TcDisplayStockount extends TcBase {
 
 		$this->assertEquals('<span class="text-success">In stock &gt; 10 pcs</span>',smarty_function_display_stockcount(array("product" => $this->products["mint_tea"]),$smarty));
 		$this->assertEquals('<span class="text-success">In stock 5 pcs</span>',smarty_function_display_stockcount(array("product" => $this->products["black_tea"]),$smarty));
+		$this->assertEquals('',smarty_function_display_stockcount(array("product" => $this->products["black_tea"], "display_nothing_if_can_be_ordered" => true),$smarty));
 	}
 }
