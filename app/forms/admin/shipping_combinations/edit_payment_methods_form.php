2fe1d51551366412aa95302112516cc69783ed88
Jaromir Tomek <jaromir.tomek@gmail.com>
2024-06-07 15:03:58 +0200

    A form in administration tuned

--- app/forms/admin/shipping_combinations/edit_payment_methods_form.php
+++ app/forms/admin/shipping_combinations/edit_payment_methods_form.php
@@ -5,7 +5,9 @@ class EditPaymentMethodsForm extends AdminForm {
 		$payment_method_choices = array();
 		foreach(PaymentMethod::FindAll() as $pm) {
 			$label = h($pm->getLabel());
-			$label .= " (".join(", ",$pm->getRegions()).")";
+			$regions = $pm->getRegions();
+			$regions = array_map(function($region){ return $region->getShortcut(); }, $regions);
+			$label .= " (".join(", ",$regions).")";
 			if(!$pm->isActive()){ $label = "<del title=\""._("neaktivní platební metoda")."\">$label</del>"; }
 			
 			$payment_method_choices[$pm->getId()] = $label;
