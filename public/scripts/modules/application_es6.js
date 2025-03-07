3f6f07aab1d65d605a588e687f6faba03b3c27c9
Jakub Tylčer <tylcer@snapps.eu>
2025-03-07 13:26:32 +0100

    JS modules using dynamic imports

--- public/scripts/modules/application_es6.js
+++ public/scripts/modules/application_es6.js
@@ -1,7 +1,24 @@
 
-import PhotoSwipeInitializer from "./photoswipe_initializer";
-import SwiperInitializer from "./swiper_initializer";
+/**
+ * Main JS module for the application. It is used mainly for loading other JS modules.
+ */
+import PhotoswipeLoader from "./photoswipeloader.js";
+import SwiperLoader from "./swiperloader.js";
 
+// Photoswipe
 
-new PhotoSwipeInitializer();
-new SwiperInitializer();
+// Check if Photoswipe is needed on the page and if so, load it
+if( document.querySelector( ".gallery__images, .iobject--picture" ||  document.querySelector( ".js_gallery_trigger" )) ) {
+  PhotoswipeLoader.load();
+} else {
+  // console.info( "No Photoswipe needed on this page" );
+}
+
+// Swiper
+
+// Check if Swiper is needed on the page and if so, load it
+if( document.querySelector( ".swiper" ) ) {
+  SwiperLoader.load();
+}else {
+  // console.info( "No Swiper needed on this page" );
+}
