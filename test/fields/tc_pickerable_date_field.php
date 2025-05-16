a7823160c904d6bca4b870162041fbdbbea34674
Jaromir Tomek <jaromir.tomek@gmail.com>
2025-05-16 17:00:41 +0200

    [PickerableDateField] Testing

--- test/fields/tc_pickerable_date_field.php
+++ test/fields/tc_pickerable_date_field.php
@@ -4,13 +4,28 @@ class TcPickerableDateField extends TcBase {
 	function test(){
 		$this->field = new PickerableDateField();
 
-		$val = $this->assertValid("2025-01-31");
-		$this->assertEquals("2025-01-31",$val);
+		$value = $this->assertValid("2023-12-22");
+		$this->assertEquals("2023-12-22",$value);
 
-		$val = $this->assertValid("03/31/2025");
-		$this->assertEquals("2025-03-31",$val);
+		$value = $this->assertValid("12/21/2023");
+		$this->assertEquals("2023-12-21",$value);
 
-		$err = $this->assertInvalid("nonsence");
-		$this->assertEquals("Enter a valid date.",$err);
+		$value = $this->assertValid(" 2023-12-24 12:33 ");
+		$this->assertEquals("2023-12-24",$value);
+
+		$err_msg = $this->assertInvalid("2023-12-33");
+		$this->assertEquals("Enter a valid date.",$err_msg);
+
+		$err_msg = $this->assertInvalid("nonsence");
+		$this->assertEquals("Enter a valid date.",$err_msg);
+	}
+
+	function test_rendering(){
+		$form = new Atk14Form();
+		$form["date"] = new PickerableDateField(array(
+			"initial" => "2023-12-33 10:51:10",
+		));
+
+		$this->assertEquals('<input required="required" type="date" name="date" class="date text form-control" id="id_date" value="2023-12-33" />',$form["date"]->as_widget());
 	}
 }
