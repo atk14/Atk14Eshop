e822ca7be083e8ad6456645c8cb351b07763311b
Michal Vojan <michal.vojan@gmail.com>
2025-08-15 15:15:24 +0200

    interface methods implemented

--- app/models/manufacturer.php
+++ app/models/manufacturer.php
@@ -1,5 +1,8 @@
 <?php
 class Manufacturer extends ApplicationModel implements Rankable, iSlug {
 
+	function getSlugPattern($lang){ return $this->getName(); }
+
+	function setRank($new_rank) { return $this->_setRank($new_rank); }
 }
 
