ce73dc083948a13eb6b73e4c47365b84ac1d57b7
Jaromir Tomek <jaromir.tomek@gmail.com>
2025-11-12 11:23:05 +0100

    Typo

--- app/controllers/searches_controller.php
+++ app/controllers/searches_controller.php
@@ -38,7 +38,7 @@ class SearchesController extends ApplicationController {
 			"offset" => $offset,
 			"limit" => 20,
 			//"damping_coefficient" => 10000000 // tlumi relevanci se starim datumu daneho dokumentu; cim vyssi cislo, tim mensi tlumeni; def. je 30
-			"boosted_types" => "card", // in sorting, cards should be priritized over other other document types
+			"boosted_types" => "card", // in sorting, cards should be prioritized over other other document types
 		);
 		if($format=="snippet"){
 			$options["offset"] = 0;
