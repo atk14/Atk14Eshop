181712c17e4c6ec21e4ce5660bc17fa32ac372cf
Jaromir Tomek <jaromir.tomek@gmail.com>
2025-11-16 12:41:29 +0100

    [Local VAT Id] User registration form tuned

--- app/forms/users/users_form.php
+++ app/forms/users/users_form.php
@@ -9,16 +9,34 @@ class UsersForm extends ApplicationForm{
 		$this->_add_firstname_lastname();
 		$this->_add_email();
 
-		$allowed_countries = $this->controller->current_region->getInvoiceCountries();
+		// Pro Slovensko je fomrular spec. potuneny,
+		// proto ho moc nechceme kombinovat s jinymi staty
+
+		$allowed_countries = $current_region_allowed_countries = $this->controller->current_region->getInvoiceCountries();
+		/*
+		// Tady zahrnujeme vsechny zeme + pripadne odstranujeme Slovensko
+		// TODO: Mozna to ani nepotrebujeme
 		if($allowed_countries!=["SK"]){
 			$allowed_countries = Region::GetInvoiceCountriesFromActiveRegions();
+			if($allowed_countries && in_array("SK",$allowed_countries)){
+				$allowed_countries = array_filter($allowed_countries,function($country){ return $country!=="SK"; });
+				$allowed_countries = array_values($allowed_countries);
+			}
 		}
+		*/
 
 		$this->_add_company_fields(["enable_vat_id_validation" => false]);
 		$this->_add_address_fields([
 			"add_note" => false,
 			"allowed_countries" => $allowed_countries,
 		]);
+
+		/*
+		// Vzhledem k zakomentovani kodu vyse, toto taky nepotrebujeme
+		if($current_region_allowed_countries && sizeof($current_region_allowed_countries)==1){
+			$this->set_initial("address_country",$current_region_allowed_countries[0]);
+		}
+		*/
 
 		$this->_add_phone();
 
