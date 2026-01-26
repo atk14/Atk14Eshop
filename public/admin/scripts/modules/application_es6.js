a3c70156dbeaf72413f871663e952afb792def13
Jakub Tylčer <tylcer@snapps.eu>
2026-01-26 14:50:09 +0100

    admin js debug

--- public/admin/scripts/modules/application_es6.js
+++ public/admin/scripts/modules/application_es6.js
@@ -2,23 +2,17 @@
 /**
  * Main JS module for the application. It is used mainly for loading other JS modules.
  */
-import PhotoswipeLoader from "./photoswipeloader.js";
 import SwiperLoader from "./swiperloader.js";
 
-// Photoswipe
 
-// Check if Photoswipe is needed on the page and if so, load it
-if( document.querySelector( ".gallery__images, .iobject--picture" ||  document.querySelector( ".js_gallery_trigger" )) ) {
-  PhotoswipeLoader.load();
-} else {
-  // console.info( "No Photoswipe needed on this page" );
-}
 
 // Swiper
-
-// Check if Swiper is needed on the page and if so, load it
-if( document.querySelector( ".swiper" ) ) {
-  SwiperLoader.load();
-}else {
-  // console.info( "No Swiper needed on this page" );
-}
+// Initialize Swiper in markdown preview areas when they are updated
+window.addEventListener( "markdownPreviewUpdated", function( e ) {
+  if( e.detail.element.querySelector( ".swiper" ) ) {
+    SwiperLoader.load().then( () => {
+      let selector = "[class='" + e.detail.parent + "'] .md-preview .swiper";
+      SwiperLoader.initSwiper( selector );
+    } );
+  }
+} );
