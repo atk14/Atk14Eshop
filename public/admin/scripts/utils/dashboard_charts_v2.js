63db1ec1e564ada6ceb9549d85587481192df7c9
Jakub Tylčer <tylcer@snapps.eu>
2026-04-29 13:11:21 +0200

    dahsboard charts js debug

--- public/admin/scripts/utils/dashboard_charts_v2.js
+++ public/admin/scripts/utils/dashboard_charts_v2.js
@@ -4,7 +4,7 @@ window.UTILS.DashboardOrdersChart = class {
 
 	color = Chart.helpers.color;
 	primaryColor = "rgb(38, 124, 217)";
-	ordersChartCtx = document.getElementById( "ordersChart" ).getContext( "2d" );
+	ordersChartCtx;
 	currentResolution = "days";
 	pageOffset = 0;
 	initialChartData;
@@ -15,7 +15,8 @@ window.UTILS.DashboardOrdersChart = class {
 	yearlyOrderStats = window.yearlyOrderStats;
 
 
-  constructor() {
+	constructor() {
+		this.ordersChartCtx = document.getElementById( "ordersChart" ).getContext( "2d" );
 		moment.locale( document.documentElement.lang );
 		Chart.register(ChartDataLabels);
 		this.initialChartData = this.getOrderDataSlice( this.dailyOrderStats, this.currentResolution, 0);
@@ -67,6 +68,7 @@ window.UTILS.DashboardOrdersChart = class {
 							time: {
 								unit: "day",
 								round: "day",
+								tooltipFormat: "LL",
 								displayFormats: {
 									day: "l", // 6. prosince 20121
 									//day: "l", // 6. 12. 2021
@@ -106,9 +108,8 @@ window.UTILS.DashboardOrdersChart = class {
 
 		// Create chart
 		this.ordersChart = new Chart( this.ordersChartCtx, this.ordersChartConfig );
-		this.toggleResolution( "days" );
 
-		this.creatUIHandlers();
+		this.createUIHandlers();
 
 		// scroll chart to the most end when in x-scrollable container
 		document.querySelector( ".chart-wrapper" ).scrollLeft = 1200;
@@ -139,7 +140,7 @@ window.UTILS.DashboardOrdersChart = class {
 		this.pageOffset = offset;
 				
 		// Get start and end datapoint index
-		let startIndex = Math.max( 0, ( dataset.length - 0 ) - ( datePeriod * ( offset + 1 ) ) );
+		let startIndex = Math.max( 0, dataset.length - ( datePeriod * ( offset + 1 ) ) );
 		let endIndex = Math.min( startIndex + datePeriod, dataset.length - 1 );
 		
 		// Start and end dates for display
@@ -170,45 +171,35 @@ window.UTILS.DashboardOrdersChart = class {
 		}
 		
 		// get slice of dataset (endIndex not included, so there is +1)
-		let slice = dataset.slice( startIndex, endIndex + 1 );
-		let output = new Object();
-		output.labels = new Array();
-		output.data = new Array();
+		const slice = dataset.slice( startIndex, endIndex + 1 );
+		const output = { labels: [], data: [] };
 
-		// prepare output obj with labels and data arrays
-		for( var i = 0; i < slice.length; i++ ) {
-			output.labels.push( slice[i].t );
-			output.data.push( slice[i].y );
+		for ( const item of slice ) {
+			output.labels.push( item.t );
+			output.data.push( item.y );
 		}
 
 		return output;
 	}
 
+	getDatasetForResolution( resolution ) {
+		switch ( resolution ) {
+			case "days":   return this.dailyOrderStats;
+			case "months": return this.monthlyOrderStats;
+			case "years":  return this.yearlyOrderStats;
+		}
+	}
+
 	// Toggle resolution (days/months/years)
 	toggleResolution( resolution ) {
 		this.currentResolution = resolution;
-		let dataArray;
-		let align = "center";
-		let unit;
-		let tooltipFormat;
-		switch ( resolution ){
-			case "days":
-				dataArray = this.dailyOrderStats;
-				tooltipFormat = "LL";
-				unit = "day";
-				align = "start"; 
-				break;
-			case "months":
-				dataArray = this.monthlyOrderStats;
-				tooltipFormat = "MMMM YYYY";
-				unit = "month";
-				break;
-			case "years":
-				dataArray = this.yearlyOrderStats;
-				tooltipFormat = "YYYY";
-				unit = "year";
-				break;
-		}
+		const dataArray = this.getDatasetForResolution( resolution );
+		const resolutionConfig = {
+			days:   { tooltipFormat: "LL",         unit: "day",   align: "start"  },
+			months: { tooltipFormat: "MMMM YYYY",  unit: "month", align: "center" },
+			years:  { tooltipFormat: "YYYY",        unit: "year",  align: "center" },
+		};
+		const { tooltipFormat, unit, align } = resolutionConfig[ resolution ];
 		let d = this.getOrderDataSlice( dataArray, resolution, 0);
 		this.ordersChart.data.datasets[0].data = d.data;
 		this.ordersChart.data.labels = d.labels;
@@ -218,60 +209,37 @@ window.UTILS.DashboardOrdersChart = class {
 		this.ordersChart.update();
 
 		// make sure proper resolution button is highhlighted
-		let btn = document.querySelector( "#chartResulutionToggle input[value=\"" + resolution + "\"]" );
+		let btn = document.querySelector( "#chartResolutionToggle input[value=\"" + resolution + "\"]" );
 		btn.checked = true;
 
 	}
 
 	// UI handlers
-	creatUIHandlers() {
+	createUIHandlers() {
 
 		// Move view left/right
-		document.querySelector( "#chartRange__left" ).addEventListener( "click", function() {
-			this.shiftOffset( 1 );
-		}.bind( this ) );
-		
-		document.querySelector( "#chartRange__right" ).addEventListener( "click", function() {
-			this.shiftOffset( -1 );
-		}.bind( this ) );
-		
+		document.querySelector( "#chartRange__left" ).addEventListener( "click", () => this.shiftOffset( 1 ) );
+		document.querySelector( "#chartRange__right" ).addEventListener( "click", () => this.shiftOffset( -1 ) );
+
 		// Resolution change buttons
-		let btns = document.querySelectorAll( "#chartResulutionToggle input" );
-		[...btns].forEach( ( input ) => {
-			input.addEventListener( "click", function( e ) {
-				this.toggleResolution( e.target.value );
-			}.bind( this ) );
+		const btns = document.querySelectorAll( "#chartResolutionToggle input" );
+		[...btns].forEach( input => {
+			input.addEventListener( "click", e => this.toggleResolution( e.target.value ) );
 		} );
 
 		// dark mode event listener
-		document.addEventListener( "darkModeChange", this.checkChartDarkMode.bind( this ) );
+		document.addEventListener( "darkModeChange", () => this.checkChartDarkMode() );
 
 		// print adjustments
-		window.addEventListener( "beforeprint", function() {
-			this.ordersChart.resize();
-		}.bind( this ) );
-		window.addEventListener( "afterprint" , function() {
-			this.ordersChart.resize();
-		}.bind( this ) );
+		window.addEventListener( "beforeprint", () => this.ordersChart.resize() );
+		window.addEventListener( "afterprint", () => this.ordersChart.resize() );
 	}
 
 
 	// Move view left/right buttons
 	shiftOffset( numPages ) {
-		let dataArray;
-		switch ( this.currentResolution ){
-			case "days":
-				dataArray = this.dailyOrderStats;
-				break;
-			case "months":
-				dataArray = this.monthlyOrderStats;
-				break;
-			case "years":
-				dataArray = this.yearlyOrderStats;
-				break;
-		}
-
-		let d = this.getOrderDataSlice( dataArray, this.currentResolution, this.pageOffset + numPages);
+		const dataArray = this.getDatasetForResolution( this.currentResolution );
+		let d = this.getOrderDataSlice( dataArray, this.currentResolution, this.pageOffset + numPages );
 		this.ordersChart.data.datasets[0].data = d.data;
 		this.ordersChart.data.labels = d.labels;
 
