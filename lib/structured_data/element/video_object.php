19e462ddf9bd109f20ca54e5c709e68d5066c55c
Michal Vojan <michal.vojan@gmail.com>
2026-05-28 14:46:25 +0200

    [StructuredData] VideoObject support

---
+++ lib/structured_data/element/video_object.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace StructuredData\Element;
+
+class VideoObject extends \StructuredData\BaseElement {
+
+	function __construct(\Video $item, $options=[]) {
+		$this->item = $item;
+	}
+
+	function toArray() {
+		$out = [
+			"@context" => "https://schema.org",
+			"@type" => "VideoObject",
+			"name" => $this->item->getTitle(),
+			"embedUrl" => $this->item->g("url"),
+		];
+
+		if ($_description = $this->item->getDescription()) {
+			$out["description"] = strip_tags((string)$_description);
+		}
+
+		if ($_thumbnail = $this->item->getPreviewImageUrl()) {
+			$out["thumbnailUrl"] = (string)$_thumbnail;
+		}
+
+		if ($_created_at = $this->item->g("created_at")) {
+			$out["uploadDate"] = date("Y-m-d", strtotime($_created_at));
+		}
+
+		return $out;
+	}
+}
