8055955916bbcd0ee217b086173a95a5857cb730
Jaromir Tomek <jaromir.tomek@gmail.com>
2026-06-29 14:21:01 +0200

    [Order Withdrawal Requests] Test fixed

--- test/models/tc_order_withdrawal_request.php
+++ test/models/tc_order_withdrawal_request.php
@@ -8,7 +8,7 @@ class TcOrderWithdrawalRequest extends TcBase {
 	function test(){
 		// S cerstvou objednavkou neni problem
 		$order = $this->_createOrder([]);
-		$order->setNewOrderStatus("processed");
+		$order->setNewOrderStatus("finished_successfully");
 		$this->assertEquals(true,OrderWithdrawalRequest::CanOrderBeWithdrawn($order,$reason));
 
 		// Nezpracovana objednavka
@@ -22,7 +22,7 @@ class TcOrderWithdrawalRequest extends TcBase {
 			"created_at" => "2026-05-01",
 		]);
 		$order->setNewOrderStatus([
-			"order_status_id" => OrderStatus::GetInstanceByCode("processed"),
+			"order_status_id" => OrderStatus::GetInstanceByCode("finished_successfully"),
 			"order_status_set_at" => "2026-05-01",
 		]);
 		$this->assertEquals(false,OrderWithdrawalRequest::CanOrderBeWithdrawn($order,$reason));
@@ -36,7 +36,7 @@ class TcOrderWithdrawalRequest extends TcBase {
 			"created_at" => (string)$date,
 		]);
 		$order->setNewOrderStatus([
-			"order_status_id" => OrderStatus::GetInstanceByCode("processed"),
+			"order_status_id" => OrderStatus::GetInstanceByCode("finished_successfully"),
 			"order_status_set_at" => (string)$date,
 		]);
 		$order->setNewOrderStatus([
@@ -53,7 +53,7 @@ class TcOrderWithdrawalRequest extends TcBase {
 			"created_at" => (string)$date,
 		]);
 		$order->setNewOrderStatus([
-			"order_status_id" => OrderStatus::GetInstanceByCode("processed"),
+			"order_status_id" => OrderStatus::GetInstanceByCode("finished_successfully"),
 			"order_status_set_at" => (string)$date,
 		]);
 		$order->setNewOrderStatus([
@@ -71,7 +71,7 @@ class TcOrderWithdrawalRequest extends TcBase {
 			"created_at" => (string)$date,
 		]);
 		$order->setNewOrderStatus([
-			"order_status_id" => OrderStatus::GetInstanceByCode("processed"),
+			"order_status_id" => OrderStatus::GetInstanceByCode("finished_successfully"),
 			"order_status_set_at" => (string)$date,
 		]);
 		$this->assertEquals(true,OrderWithdrawalRequest::CanOrderBeWithdrawn($order,$reason));
@@ -84,10 +84,27 @@ class TcOrderWithdrawalRequest extends TcBase {
 			"created_at" => (string)$date,
 		]);
 		$order->setNewOrderStatus([
-			"order_status_id" => OrderStatus::GetInstanceByCode("processed"),
+			"order_status_id" => OrderStatus::GetInstanceByCode("finished_successfully"),
 			"order_status_set_at" => (string)$date,
 		]);
 		$this->assertEquals(false,OrderWithdrawalRequest::CanOrderBeWithdrawn($order,$reason));
 		$this->assertEquals(_("Objednávku již není možné vrátit."),$reason);
+	}
+
+	function _createOrder($values =  []){
+		$delivery = $this->delivery_methods["dpd"];
+		$payment = $this->payment_methods["credit_card"];
+
+		$values += [
+			"user_id" => null,
+			"region_id" => Region::GetDefaultRegion(),
+			"delivery_method_id" => $delivery,
+			"delivery_fee_incl_vat" => $delivery->getPriceInclVat(),
+			"payment_method_id" => $payment,
+			"payment_fee_incl_vat" => $payment->getPriceInclVat(),
+			"price_to_pay" => 200.0
+		];
+	
+		return Order::CreateNewRecord($values);
 	}
 }
