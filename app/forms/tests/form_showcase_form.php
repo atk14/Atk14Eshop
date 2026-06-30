95d158c603604d83a31c9f1e38c8378119c3d8eb
Jaromir Tomek <jaromir.tomek@gmail.com>
2026-06-30 15:43:31 +0200

    Testing

--- app/forms/tests/form_showcase_form.php
+++ app/forms/tests/form_showcase_form.php
@@ -2,6 +2,14 @@
 class FormShowcaseForm extends TestsForm {
 
 	function set_up(){
+		$this->add_field("fruits", new MultipleChoiceField([
+			"choices" => [
+				"apple" => "Apple",
+				"orange" => "Orange",
+				"banana" => "Banana",
+			],
+			"help_text" => "MultipleChoiceField",
+		]));
 		$this->add_field("colors", new MultipleChoiceField([
 			"choices" => [
 				"red" => "Red",
@@ -9,6 +17,10 @@ class FormShowcaseForm extends TestsForm {
 				"blue" => "Blue",
 			],
 			"widget" => new CheckboxSelectMultiple(),
+			"help_text" => "MultipleChoiceField with CheckboxSelectMultiple",
+		]));
+		$this->add_field("boolean", new BooleanField([
+			"help_text" => "BooleanField",
 		]));
 		$this->set_button_text(_("Send"));
 	}
