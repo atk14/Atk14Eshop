da72b4b9d5c8497e016220a3db1036bb80039bd7
Jaromir Tomek <jaromir.tomek@gmail.com>
2026-07-03 10:44:12 +0200

    Testing

--- test/controllers/tc_sitemaps.php
+++ test/controllers/tc_sitemaps.php
@@ -5,12 +5,15 @@ class TcSitemaps extends TcBase {
 		$client = $this->client;
 		
 		$client->get("sitemaps/index");
+		$this->assertEquals("application/xml",$client->getContentType());
 		$this->assertStringContains("public, max-age=",$client->getResponseHeader("Cache-Control"));
 
 		$client->get("sitemaps/detail");
+		$this->assertEquals("text/html",$client->getContentType());
 		$this->assertStringContains("private",$client->getResponseHeader("Cache-Control"));
 
 		$client->get("sitemaps/detail",["format" => "xml"]);
+		$this->assertEquals("application/xml",$client->getContentType());
 		$this->assertStringContains("public, max-age=",$client->getResponseHeader("Cache-Control"));
 	}
 }
